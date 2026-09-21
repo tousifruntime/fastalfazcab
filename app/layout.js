cb2b9e5e6bc4d9920b07de1c854895f647d1ee08
tousif run time <tousifruntime@gmail.com>
2026-09-21 15:47:46 +0530

    Add Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
  
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { GoogleAnalytics } from "@next/third-parties/google";
  
 
 export default function RootLayout({ children }) {
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
     <html lang="en"  className="h-full antialiased"  >
       <body className="min-h-full flex flex-col">
         {children}
+        <GoogleAnalytics gaId="G-RQBM3BXV91" />
         <Toaster />
       </body>
     </html>
